7c1e9141d879cdc93d51654aaae98dadf60f29f3
younggeeks <younggeeks101@gmail.com>
2018-05-04 00:16:53 +0300

    [Chore] add UserSerializer for converting models to native python datatypes

--- wger/core/api/serializers.py
+++ wger/core/api/serializers.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with Workout Manager.  If not, see <http://www.gnu.org/licenses/>.
-
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from wger.core.models import (
@@ -30,8 +30,41 @@ class UserprofileSerializer(serializers.ModelSerializer):
     '''
     Workout session serializer
     '''
+
     class Meta:
         model = UserProfile
+
+
+# helper function to ensure that field is not empty
+def requiredField(value):
+    if not value:
+        raise serializers.ValidationError('This field is required')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    API USER Serializer
+    """
+    # marking email as required
+    email = serializers.EmailValidator()
+
+    def create(self, validated_data):
+        creator = self.context['request'].user
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        user.userprofile.created_by = creator.username
+        user.userprofile.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
 
 class UsernameSerializer(serializers.Serializer):
@@ -45,6 +78,7 @@ class LanguageSerializer(serializers.ModelSerializer):
     '''
     Language serializer
     '''
+
     class Meta:
         model = Language
 
@@ -53,6 +87,7 @@ class DaysOfWeekSerializer(serializers.ModelSerializer):
     '''
     DaysOfWeek serializer
     '''
+
     class Meta:
         model = DaysOfWeek
 
@@ -61,6 +96,7 @@ class LicenseSerializer(serializers.ModelSerializer):
     '''
     License serializer
     '''
+
     class Meta:
         model = License
 
@@ -69,6 +105,7 @@ class RepetitionUnitSerializer(serializers.ModelSerializer):
     '''
     Repetition unit serializer
     '''
+
     class Meta:
         model = RepetitionUnit
 
@@ -77,5 +114,6 @@ class WeightUnitSerializer(serializers.ModelSerializer):
     '''
     Weight unit serializer
     '''
+
     class Meta:
         model = WeightUnit
