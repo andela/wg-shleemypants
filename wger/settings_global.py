8d5a20e4f91444ff5f6d73efc93f2358c91d0efc
mirr254 <sammysteppa90@gmail.com>
2018-05-10 12:48:00 +0300

    [Chore ] Social auth inclusion and setting up

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #for social logins
+    'social_django',
 
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
@@ -129,6 +131,11 @@ MIDDLEWARE_CLASSES = (
 )
 
 AUTHENTICATION_BACKENDS = (
+    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+    'social_core.backends.google.GoogleOpenId',  # for Google authentication
+    'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+    'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+ 
     'django.contrib.auth.backends.ModelBackend',
     'wger.utils.helpers.EmailAuthBackend'
 )
