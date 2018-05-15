4f163432e2139cbf980a8fbc639cfb2b0dd3a38d
Rodgers-M <mkishagha@gmail.com>
2018-05-15 08:14:16 +0300

    [fix]: get the number of days in a month use this value to calculate the last date in the month using time delta

--- wger/utils/month_range.py
+++ wger/utils/month_range.py
@@ -7,6 +7,6 @@ def get_month_range(start_date=None):
     '''calculate the month range'''
     if start_date is None:
         start_date = date.today().replace(day=1)
-    days_in_month = calendar.monthrange(start_date.year, start_date.month)
-    end_date = start_date + timedelta(days=days_in_month)
+    month_range = calendar.monthrange(start_date.year, start_date.month)
+    end_date = start_date + timedelta(days=month_range[1]-1)
     return (start_date, end_date)
